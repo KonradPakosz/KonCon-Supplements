2ace0d369918e262f1f238ae32257dcc73d81b57
EC2 Default User <ec2-user@ip-172-31-32-254.ec2.internal>
2019-11-11 21:40:48 +0000

    style changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+#...
+Product.create!(
+    title: '2kg KonCon Whey Protein',
+    description: %{<p>'2,000g of premium whey protein powder. Fucking nooice'</p>},
+    image_url: 'https://images.hollandandbarrettimages.co.uk/productimages/HB/370/099183_A.jpg', 
+    price: 29.99)
+#...
