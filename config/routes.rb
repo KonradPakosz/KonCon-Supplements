e204f7bb7873c0f69035f2ed1a67811ee2e91a4e
EC2 Default User <ec2-user@ip-172-31-32-254.ec2.internal>
2019-11-14 23:45:11 +0000

    added static pages home and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  root 'static_pages#home'
+  get '/about' => 'static_pages#about'
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
