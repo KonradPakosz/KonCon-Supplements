d12a1609c08b3f6d9804a581da1bc26f40037d7c
Conor <conormoloneycm@gmail.com>
2019-11-15 17:42:03 +0000

    Added products html and updates the static page controller and routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
   get '/about' => 'static_pages#about'
+  get 'products' => 'static_pages#products'
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
