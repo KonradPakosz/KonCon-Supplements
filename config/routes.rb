a4602fba490e0d880e858bc494326f9d77d5908c
EC2 Default User <ec2-user@ip-172-31-32-254.ec2.internal>
2019-11-17 15:33:50 +0000

    login logout functions and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   
+  devise_for :users
   root 'static_pages#home'
   get '/about' => 'static_pages#about'
   get '/login' => 'user#login' 
   get '/logout' => 'user#logout'
+
+  root :to => 'site#home'
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
