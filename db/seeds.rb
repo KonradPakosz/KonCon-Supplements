9d0c1b0c2275c231d7d4b327737f3b926338eaa0
EC2 Default User <ec2-user@ip-172-31-32-254.ec2.internal>
2019-11-18 19:27:25 +0000

    filled some seeds and updated cart table

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,35 @@
 Product.delete_all
 #...
 Product.create!(
-    title: '2kg KonCon Whey Protein',
-    description: %{<p>'2,000g of premium whey protein powder. Fucking nooice'</p>},
-    image_url: 'https://images.hollandandbarrettimages.co.uk/productimages/HB/370/099183_A.jpg', 
+    title: '2.5kg Whey Protein',
+    description: %{<p>'Whey Protein is a premium source of Whey Protein Concentrate, which contains 82% protein (based on unflavoured) and is sourced from a leading European dairy. Its unrivalled quality ensures that it is naturally rich in BCAA (5.3g per serving, based on unflavoured) – making it the perfect choice for anyone looking to increase their protein intake.'</p>},
+    image_url: 'http://www.de-omairah.com/wp-content/uploads/2017/03/Sample.jpg', 
+    price: 39.99)
+Product.create!(
+    title: '1kg Whey Protein',
+    description: %{<p>'Whey Protein is a premium source of Whey Protein Concentrate, which contains 82% protein (based on unflavoured) and is sourced from a leading European dairy. Its unrivalled quality ensures that it is naturally rich in BCAA (5.3g per serving, based on unflavoured) – making it the perfect choice for anyone looking to increase their protein intake.'</p>},
+    image_url: 'http://www.de-omairah.com/wp-content/uploads/2017/03/Sample.jpg', 
+    price: 19.99)
+Product.create!(
+    title: '100g BCAA',
+    description: %{<p>'BCAA are rapidly becoming one of the most popular sports nutrition products available. It is often used during workouts, or consumed throughout the day, helping to promote muscle growth and reduce muscle soreness. BCAA also improve recovery times, helping you to exercise more and at optimal capacity.'</p>},
+    image_url: 'http://www.de-omairah.com/wp-content/uploads/2017/03/Sample.jpg', 
+    price: 9.99)
+Product.create!(
+    title: '500g BCAA',
+    description: %{<p>'BCAA are rapidly becoming one of the most popular sports nutrition products available. It is often used during workouts, or consumed throughout the day, helping to promote muscle growth and reduce muscle soreness. BCAA also improve recovery times, helping you to exercise more and at optimal capacity.'</p>},
+    image_url: 'http://www.de-omairah.com/wp-content/uploads/2017/03/Sample.jpg',
     price: 29.99)
+Product.create!(
+    title: '500g Creatine Monohydrate',
+    description: %{<p>'Creatine Monohydrate is the most widely used sports nutrition supplement in existence. As an example, it is estimated that over 90% of athletes at the 2012 Olympics supplemented with Creatine! 
+    Why is Creatine so popular? Well, this is for good reason. Creatine is research proven to increase physical performance in successive bursts of short-term, high intensity exercise. This makes it the perfect supplement for all kinds of athletes, including: bodybuilders, powerlifters, footballers, rugby players, sprinters – and pretty much anyone participating in high intensity exercise.'</p>},
+    image_url: 'http://www.de-omairah.com/wp-content/uploads/2017/03/Sample.jpg',
+    price: 9.99)
+Product.create!(
+    title: '1kg Creatine Monohydrate',
+    description: %{<p>'Creatine Monohydrate is the most widely used sports nutrition supplement in existence. As an example, it is estimated that over 90% of athletes at the 2012 Olympics supplemented with Creatine! 
+    Why is Creatine so popular? Well, this is for good reason. Creatine is research proven to increase physical performance in successive bursts of short-term, high intensity exercise. This makes it the perfect supplement for all kinds of athletes, including: bodybuilders, powerlifters, footballers, rugby players, sprinters – and pretty much anyone participating in high intensity exercise.'</p>},
+    image_url: 'http://www.de-omairah.com/wp-content/uploads/2017/03/Sample.jpg',
+    price: 14.99)
 #...
