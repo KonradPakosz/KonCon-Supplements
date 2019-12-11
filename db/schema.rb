6a131b71bc0602e064314c5bc9be606ceac283d0
Conor <conormoloneycm@gmail.com>
2019-12-11 15:46:51 +0000

    Added dob, name and gender fields for users on sign up and to edit in their profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191209212141) do
+ActiveRecord::Schema.define(version: 20191211153650) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -78,6 +78,8 @@ ActiveRecord::Schema.define(version: 20191209212141) do
     t.datetime "updated_at",                          null: false
     t.boolean  "admin"
     t.string   "name"
+    t.datetime "date_of_birth"
+    t.boolean  "is_female"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
